a2d9cf01f436ab152cf690adf749259eed9b6d1c
pthagnar <robert.harry.nicodemus.williams@gmail.com>
2015-01-12 19:08:40 +0000

    Thoughts

---
+++ phonology.py
@@ -0,0 +1,37 @@
+def dummy():
+    """
+what should a phonology store?
+
+PHONEMES, obviously -- a set of Phones as The Phonemic Vowels, from which allophonic
++ suprasegmental variants are derived
+
+but also systems
+ -- allophony
+ -- suprasegmentals
+ -- system analysis -- systemzwang
+
+Vowels
+------
+
+dhok has compiled a big list + classified vowel systems at http://www.incatena.org/viewtopic.php?f=7&t=41583
+classifying a collection of vowels like this would be a start
+
+something which goes 'lol ur vowels are the same as in <list of languages here>'
+this should be pretty easy, i think, since it is just looking at sets of vowels
+
+short + long vowel systems
+and vowel subsystems generally c.f. nort: http://www.incatena.org/viewtopic.php?p=1047560#p1047560
+
+actually, get nort to consult, he is a vowelf*cker
+
+dipthongs????
+
+Consonants
+-----------
+
+?????
+
+    """
+    while 1:
+        print("ALLAHU AKBAR")
+
